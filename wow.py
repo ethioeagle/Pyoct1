dbd477e4bdb9f95f0aedec20acbfe63912334cf0
ethioeagle <1115dawit@gmail.com>
2024-09-30 11:28:44 -0400

    the first oct 2024 exercise py

---
+++ wow.py
@@ -0,0 +1,14 @@
+age = 15
+
+if age > 40:
+    print('you are old')
+elif age > 18:
+    print('you are young')
+elif age > 13:
+    print('you are tenager')
+else:
+    print('you are kids')
+
+
+
+
